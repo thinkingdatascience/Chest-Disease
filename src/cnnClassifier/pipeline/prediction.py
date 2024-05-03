2658240dfa099d6b3b17ab3b51290d2dfe7833c2
Farhan Shaikh <thinkingdatascience@gmail.com>
2024-05-03 06:47:45 +0300

    prediction added

---
+++ src/cnnClassifier/pipeline/prediction.py
@@ -0,0 +1,28 @@
+import os
+import numpy as np
+import tensorflow as tf
+
+
+class PredictionPipeline:
+    def __init__(self, filename) -> None:
+        self.filename = filename
+
+    def predict(self):
+
+        image = tf.keras.preprocessing.image
+
+        model = tf.keras.models.load_model(os.path.join("model", "model.h5"))
+
+        imagename = self.filename
+        test_image = image.load_img(imagename, target_size=(224, 224))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis=0)
+        result = np.argmax(model.predict(test_image), axis=1)
+        print(result)
+
+        if result[0] == 1:
+            prediction = "Normal"
+            return [{"image": prediction}]
+        else:
+            prediction = "adenocarcinoma Cancer"
+            return [{"image": prediction}]
